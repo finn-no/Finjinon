50fdb284beed827454622f83d63be3228968be2f
johsoren <johan.sorensen@finn.no>
2015-06-19 12:27:06 +0200

    Scale in the inserted collection view cell

--- Finjinon/PhotoCaptureViewController.swift
+++ Finjinon/PhotoCaptureViewController.swift
@@ -43,7 +43,7 @@ public class PhotoCaptureViewController: UIViewController {
         containerView.frame = CGRect(x: 0, y: view.frame.height-76-collectionViewHeight, width: view.frame.width, height: 76+collectionViewHeight)
         view.addSubview(containerView)
 
-        let layout = UICollectionViewFlowLayout()
+        let layout = PhotoCollectionViewLayout()
         collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: containerView.bounds.width, height: collectionViewHeight), collectionViewLayout: layout)
 
         layout.scrollDirection = .Horizontal
@@ -156,5 +156,38 @@ private class PhotoCollectionViewCell: UICollectionViewCell {
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+private class PhotoCollectionViewLayout: UICollectionViewFlowLayout {
+    var insertedIndexPaths: [NSIndexPath] = []
+
+    override func prepareForCollectionViewUpdates(updateItems: [AnyObject]!) {
+        super.prepareForCollectionViewUpdates(updateItems)
+
+        insertedIndexPaths.removeAll(keepCapacity: true)
+
+        for update in updateItems as! [UICollectionViewUpdateItem] {
+            switch update.updateAction {
+            case .Insert:
+                insertedIndexPaths.append(update.indexPathAfterUpdate!)
+            default:
+                return
+            }
+        }
+    }
+
+    override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        let attrs = super.initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath)
+
+        if contains(insertedIndexPaths, itemIndexPath) {
+            if let attrs = attrs {
+                let transform = CATransform3DTranslate(attrs.transform3D, attrs.frame.midX - self.collectionView!.frame.midX, attrs.frame.midY - self.collectionView!.frame.midY, 0)
+                attrs.transform3D = CATransform3DScale(transform, 0.001, 0.001, 1)
+            }
+        }
+
+        return attrs
     }
 }
