6aeec5bfcd8534873bc7056298c0bfc2f22d6501
Elvis Nuñez <elvisnunez@me.com>
2017-11-28 22:12:09 +0100

    Add carthage support

---
+++ Example/Finjinon/Finjinon.h
@@ -0,0 +1,19 @@
+//
+//  Finjinon.h
+//  Finjinon
+//
+//  Created by Elvis Nuñez on 28/11/2017.
+//  Copyright © 2017 FINN.no. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Finjinon.
+FOUNDATION_EXPORT double FinjinonVersionNumber;
+
+//! Project version string for Finjinon.
+FOUNDATION_EXPORT const unsigned char FinjinonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Finjinon/PublicHeader.h>
+
+
