cefe4d38917df738b04526eb0f059662c77babd7
Jonas Olsson <osanoj@users.noreply.github.com>
2024-07-02 17:47:02 +0200

    Fix comment

--- Sources/Finjinon.swift
+++ Sources/Finjinon.swift
@@ -7,7 +7,7 @@ import Foundation
 public struct Finjinon {
     static var configuration: FinjinonConfiguration!
 
-    /// Required setup to be able to use SearchQuest
+    /// Required setup to be able to use Finjinon
     public static func setup(_ configuration: FinjinonConfiguration) {
         self.configuration = configuration
     }
